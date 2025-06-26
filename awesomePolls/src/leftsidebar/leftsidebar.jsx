9dc238a88485c11e97ec65d498bfcaa80f71d410
Miraj <mirajsheikh951@gmail.com>
2025-06-27 02:12:38 +0530

    Icons Added

--- awesomePolls/src/leftsidebar/leftsidebar.jsx
+++ awesomePolls/src/leftsidebar/leftsidebar.jsx
@@ -4,70 +4,93 @@ import useUserContext from "../pollProvider"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
+import profile from "../assets/profile.svg"
+import profiledark from "../assets/profiledark.svg"
+import explore from "../assets/explore.svg"
+import exploredark from "../assets/exploredark.svg"
+import mypolls from "../assets/mypolls.svg"
+import mypollsdark from "../assets/mypollsdark.svg"
+import newpoll from "../assets/newpoll.svg"
+import newpolldark from "../assets/newpolldark.svg"
+import settings from "../assets/settings.svg"
+import settingsdark from "../assets/settingsdark.svg"
+import favorites from "../assets/favorites.svg"
+import favoritesdark from "../assets/favoritesdark.svg"
+import mostvoted from "../assets/mostvoted.svg"
+import mostvoteddark from "../assets/mostvoteddark.svg"
+import logout from "../assets/logout.svg"
+import logoutdark from "../assets/logoutdark.svg"
+import expand from "../assets/expand.svg"
+import expanddark from "../assets/expanddark.svg"
+import collapse from "../assets/collapse.svg"
+import collapsedark from "../assets/collapsedark.svg"
+
 const LeftSideBar = () => {
 
-  const { theme, setUser } = useUserContext()
+	const { theme, setUser } = useUserContext()
 
-  const navigate = useNavigate()
+	const navigate = useNavigate()
 
-  const styles = theme ? light : dark
+	const styles = theme ? light : dark
 
-  const [collapsed, setCollapsed] = useState(true)
-  const [width, setWidth] = useState(window.innerWidth)
+	const [collapsed, setCollapsed] = useState(true)
+	const [width, setWidth] = useState(window.innerWidth)
 
-  function getWindowWidth(){
-    setWidth(window.innerWidth)
-  }
+	function getWindowWidth() {
+		setWidth(window.innerWidth)
+	}
 
-  useEffect(() => {
+	useEffect(() => {
 
-    window.addEventListener("resize", getWindowWidth)
+		window.addEventListener("resize", getWindowWidth)
 
-    return () => {window.removeEventListener("resize", getWindowWidth)}
+		return () => { window.removeEventListener("resize", getWindowWidth) }
 
-  }, [])
+	}, [])
 
-  useEffect(() => {
-    if(width < 700){
-      setCollapsed(true)
-    }
-  }, [width])
+	useEffect(() => {
+		if (width < 700) {
+			setCollapsed(true)
+		}
+	}, [width])
 
-  return(
+	return (
 
-    <div className={collapsed ? styles.collapsedSideBar : styles.expandedSideBar} style={{zIndex: "2"}}>
+		<div className={collapsed ? styles.collapsedSideBar : styles.expandedSideBar} style={{ zIndex: "2" }}>
 
-      <div className={styles.filters}>
-        
-      </div>
-     
-      <div className={styles.settings}>
-        <div 
-          onClick={() => setCollapsed(!collapsed)} 
-          style={{justifyContent: `${collapsed ? "start" : "end"}`}}>
-          <img src="https://placehold.co/80" />
-        </div>
-        <div>{collapsed ? <img src="https://placehold.co/40" /> : <button>Profile</button>}</div>
-        <div onClick={() => navigate("/mypolls")}>{collapsed ? <img src="https://placehold.co/40" /> : <button>My Polls</button>}</div>
-        <div onClick={() => navigate("/polls")}>{collapsed ? <img src="https://placehold.co/40" /> : <button>Explore</button>}</div>
-        <div onClick={() => navigate("/new")}>{collapsed ? <img src="https://placehold.co/40" /> : <button>New Poll</button>}</div>
-        <div>{collapsed ? <img src="https://placehold.co/40" /> : <button>Settings</button>}</div>
-        <div>{collapsed ? <img src="https://placehold.co/40" /> : <button>Favorites</button>}</div>
-        <div>{collapsed ? <img src="https://placehold.co/40" /> : <button>Most Voted Polls</button>}</div>
-        <div>{collapsed 
-          ? <img src="https://placehold.co/40" /> 
-          : <button onClick={() => {
-            setUser(null)
-            navigate("/")
-          }
-          }>Logout</button>}
-        </div>
-      </div>
+			<div className={styles.filters}>
 
+			</div>
 
-    </div>
+			<div className={styles.settings}>
+				<div
+					onClick={() => setCollapsed(!collapsed)}
+					style={{ justifyContent: `${collapsed ? "start" : "end"}` }}>
+					<img src={collapsed
+						? theme ? expand : expanddark
+						: theme ? collapse : collapsedark
+					} />
+				</div>
+				<div>{collapsed ? <img src={theme ? profile : profiledark} /> : <button>Profile</button>}</div>
+				<div onClick={() => navigate("/mypolls")}>{collapsed ? <img src={theme ? mypolls : mypollsdark} /> : <button>My Polls</button>}</div>
+				<div onClick={() => navigate("/polls")}>{collapsed ? <img src={theme ? explore : exploredark} /> : <button>Explore</button>}</div>
+				<div onClick={() => navigate("/new")}>{collapsed ? <img src={theme ? newpoll : newpolldark} /> : <button>New Poll</button>}</div>
+				<div>{collapsed ? <img src={theme ? settings : settingsdark} /> : <button>Settings</button>}</div>
+				<div>{collapsed ? <img src={theme ? favorites : favoritesdark} /> : <button>Favorites</button>}</div>
+				<div>{collapsed ? <img src={theme ? mostvoted : mostvoteddark} /> : <button>Most Voted Polls</button>}</div>
+				<div>{collapsed
+					? <img src={theme ? logout : logoutdark} />
+					: <button onClick={() => {
+						setUser(null)
+						navigate("/")
+					}
+					}>Logout</button>}
+				</div>
+			</div>
 
-  )
+		</div>
+
+	)
 
 }
 
